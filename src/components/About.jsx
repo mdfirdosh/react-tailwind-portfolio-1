5adce062d05856a3832e7a6e115320307cddee04
mdfirdosh <mdfirdosh.ws@gmail.com>
2022-09-12 07:36:51 +0530

    Version 1.0

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b
+     from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem a distinctio hic quo. Praesentium quod corrupti expedita eveniet ea qui, sed natus dolores dolor id repudiandae quisquam necessitatibus exercitationem inventore, saepe dignissimos voluptatem repellendus ipsum aperiam vitae doloremque! Amet cumque harum vero animi aperiam dolores excepturi explicabo dolorum pariatur quas.s
+            </p>
+            <br />
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quaerat officia commodi enim quo totam veniam quod ad atque tempore id soluta vel incidunt, dolor sed explicabo assumenda? Nulla, earum consequatur. Quas quisquam consequatur est nemo obcaecati iusto, eligendi sint nesciunt blanditiis beatae corrupti, veritatis vitae minus, ullam a aliquam.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
